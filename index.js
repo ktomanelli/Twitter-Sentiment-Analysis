855c062b6337b10e2ee27f0df6940b5aaf1409f2
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-24 23:39:29 -0400

    able to continually get tweets and compute new sentiment averages

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { getTweets } = require('./twitter');
 const { detectSentiment } = require('./sentiment');
 
-const getBatch = async searchQuery => {
+const getBatch = async (q, since_id = null, count = 10) => {
   let tweets;
   const tweetObjects = [];
 
   try {
-    tweets = await getTweets({ q: searchQuery, count: 10 });
+    tweets = await getTweets({ q, count, since_id });
   } catch (err) {
     console.log({ err, message: 'twitter error' });
   }
@@ -36,7 +36,22 @@ const getBatch = async searchQuery => {
   return tweetObjects;
 };
 
-const getAverages = tweetObjects => {
+const getAverages = (tweetObjects, avg = null) => {
+  let averages;
+  let length;
+  if (avg) {
+    length = avg.length + tweetObjects.length;
+    averages = {
+      POS: avg.POS * avg.length,
+      NEG: avg.NEG * avg.length,
+      NEU: avg.NEU * avg.length,
+      MIX: avg.MIX * avg.length,
+    };
+  } else {
+    length = tweetObjects.length;
+    averages = { POS: 0, NEG: 0, NEU: 0, MIX: 0 };
+  }
+
   const totals = tweetObjects.reduce(
     (acc, { score }) => ({
       POS: score.Positive + acc.POS,
@@ -47,16 +62,26 @@ const getAverages = tweetObjects => {
     { POS: 0, NEG: 0, NEU: 0, MIX: 0 }
   );
 
-  const averages = {
-    POS: totals.POS / tweetObjects.length,
-    NEG: totals.NEG / tweetObjects.length,
-    NUE: totals.NEU / tweetObjects.length,
-    MIX: totals.MIX / tweetObjects.length,
+  averages = {
+    POS: totals.POS / length,
+    NEG: totals.NEG / length,
+    NUE: totals.NEU / length,
+    MIX: totals.MIX / length,
   };
-  console.log(averages);
+  return { averages, length };
 };
 
-async function start() {
-  console.log(await getBatch('epstein'));
-}
-start();
+const areTheyCancelled = async q => {
+  let batch = await getBatch(q);
+  let last = batch[batch.length - 1].id;
+  let avg = getAverages(batch);
+  console.log(avg);
+  while (true) {
+    batch = await getBatch(q, last);
+    last = batch[batch.length - 1].id;
+    avg = getAverages(batch, avg);
+    console.log(avg);
+  }
+};
+
+areTheyCancelled('epstein');
